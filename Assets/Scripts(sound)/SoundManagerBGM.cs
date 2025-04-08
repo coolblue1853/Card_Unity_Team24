272e6cca41f26c61f6ac219a8ba4ffe73ce61cdf
A7442 <parkjuna1121@naver.com>
2025-04-08 19:59:25 +0900

    feet : 볼륨 추가 및 일부 코드 개선
    볼륨을 관리할 수 있는 슬라이드 추가
    
    SoundManager에서 BGM과 SFX로 나눔

---
+++ Assets/Scripts(sound)/SoundManagerBGM.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManagerBGM : MonoBehaviour
+{
+    public static SoundManagerBGM Instance;
+
+
+    AudioSource audiosource;
+
+    public AudioClip MainBgm;
+    public AudioClip GameBgm;
+    public AudioClip FeverBgm;
+
+    public AudioClip WarningSound;
+
+
+    float time = 20.0f;
+    string currentScene;
+    bool Iswarningplay = false;
+    float playtime;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().name;
+        audiosource = GetComponent<AudioSource>();
+
+        if (currentScene == "Start")
+        {
+            audiosource.clip = this.MainBgm;
+            audiosource.Play();
+        }
+        else if(currentScene == "Game")
+        {
+            audiosource.clip = this.GameBgm;
+            audiosource.Play();
+        }
+    }
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (currentScene == "Game")
+        {
+            if (time <= 7.0f && time>0)
+            {
+                if(!Iswarningplay)
+                {
+                    audiosource.PlayOneShot(WarningSound);
+                    Iswarningplay = true;
+                    playtime = time;
+                }
+                if(playtime-time>=0.8f)
+                {
+                    Iswarningplay = false;
+                }
+            }
+            else if(time <=0)
+            {
+                audiosource.Stop();
+            }
+        }
+    }
+    public void FevertimeBgm()
+    {
+        audiosource.clip = this.FeverBgm;
+        audiosource.Play();
+    }
+}

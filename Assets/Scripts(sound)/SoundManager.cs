49ec5235749cab6e1ee6602f452420dde1161d0f
A7442 <parkjuna1121@naver.com>
2025-04-08 11:50:54 +0900

    feet : sound추가 및 함수 추가
    새로운 sound 2개 추가
    Start에 if문 추가 : Start Scene Bgm과 Game Scene Bgm구분
    여러 sound불러오는 함수 추가

--- Assets/Scripts(sound)/SoundManager.cs
+++ Assets/Scripts(sound)/SoundManager.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public bool isstratGame = false;
+
     public static SoundManager Instance; 
+
     AudioSource audiosource;
+
     public AudioClip MainBgm;
+    public AudioClip GameBgm;
+    public AudioClip FlipSound;
+    public AudioClip MatchSound;
+    public AudioClip FailSound;
 
     private void Awake()
     {
@@ -21,7 +29,28 @@ public class SoundManager : MonoBehaviour
     void Start()
     {
         audiosource = GetComponent<AudioSource>();
-        audiosource.clip = this.MainBgm;
-        audiosource.Play();
+        if (!isstratGame)
+        {
+            audiosource.clip = this.MainBgm;
+            audiosource.Play();
+        }
+        else if(isstratGame)
+        {
+            audiosource.clip = this.GameBgm;
+            audiosource.Play();
+        }
+    }
+
+    public void FlipSd()
+    {
+        audiosource.PlayOneShot(FlipSound);
+    }
+    public void MatchSd()
+    {
+        audiosource.PlayOneShot(MatchSound);
+    }
+    public void FailSd()
+    {
+        audiosource.PlayOneShot(FailSound);
     }
 }

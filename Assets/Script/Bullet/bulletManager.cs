3f180f59cf323c86fc52d25a99dd982bd206252d
chae_kkung <ycy0109@naver.com>
2025-04-08 15:14:02 +0900

    feat : 총알 생성 위치와 발사 방향 계산
    총알을 생성하는 위치를 화면 밖으로 랜덤하게 지정하고, 발사 방향을 50%의 확률로 플레이어에게, 나머지 50%의 확률로 랜덤하게(화면 안으로) 발사하는 함수를 작성했습니다.

--- Assets/Script/Bullet/bulletManager.cs
+++ Assets/Script/Bullet/bulletManager.cs
@@ -6,6 +6,8 @@ public class bulletManager : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public GameObject[] bullets;
+    public Transform player;
+
     int pivot = 0;
     void Start()
     {
@@ -23,7 +25,51 @@ public class bulletManager : MonoBehaviour
     {
         yield return new WaitForSeconds(0.1f);
         bullets[pivot].SetActive(true);
-        pivot++;
+
+        Vector2 spawn = GetRandomPosition();
+        bulletPrefab.transform.position = spawn;
+
+        Vector2 direction;
+
+        if(Random.value < 0.5f)
+        {
+            Vector2 targetPos = player.transform.position;
+            direction = (targetPos - spawn).normalized;
+        }
+        else
+        {
+            Vector2 targetPos = new Vector2(Random.Range(-3, 3), Random.Range(-5, 5));
+            direction = (targetPos - spawn).normalized;
+        }
+        
+        Vector2 GetRandomPosition()
+        {
+            int num = Random.Range(1,5);
+
+            Vector2 pos = new Vector2(0,0);
+
+                switch (num)
+                {
+                case 1:
+                    pos = new Vector2(Random.Range(-3, 3), Random.Range(5, 6));
+                    break;
+
+                case 2:
+                    pos = new Vector2(Random.Range(-3, 3), Random.Range(-6, -5));
+                    break ;
+
+                case 3:
+                    pos = new Vector2(Random.Range(-4, -3), Random.Range(-5, 5));
+                    break ;
+
+                case 4:
+                    pos = new Vector2(Random.Range(3, 4), Random.Range(-5, 5));
+                    break ;
+                }
+            return pos;
+        }
+
+            pivot++;
 
         if(pivot == bullets.Length)
             pivot = 0;

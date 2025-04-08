68054bc1ec0e527c9293090633735b29af3fd78c
chae_kkung <ycy0109@naver.com>
2025-04-08 11:24:41 +0900

    feat : bullet을 생성
    오브젝트 풀링 기법을 활용해 bullet을 생성하는 코드를 작성했습니다.

---
+++ Assets/Script/Bullet/bulletManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletManager : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public GameObject[] bullets;
+    int pivot = 0;
+    void Start()
+    {
+        bullets = new GameObject[100];
+        for(int i = 0; i < bullets.Length; i++)
+        {
+            GameObject gameObject = Instantiate(bulletPrefab);
+            bullets[i] = gameObject;
+            gameObject.SetActive(false);
+        }
+        StartCoroutine(Shoot());
+    }
+
+    IEnumerator Shoot()
+    {
+        yield return new WaitForSeconds(0.1f);
+        for(int i = 0; i < bullets.Length; i++)
+        {
+            bullets[i].SetActive(true);
+            pivot++;
+        }
+        if(pivot == bullets.Length)
+            pivot = 0;
+
+        StartCoroutine(Shoot());
+    }
+}

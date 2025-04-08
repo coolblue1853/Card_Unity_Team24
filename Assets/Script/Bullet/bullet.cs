b494af87a2139786029f915baeb2b170c7f10718
chae_kkung <ycy0109@naver.com>
2025-04-08 16:46:10 +0900

    feat : 총알 발사
    총알의 속도를 지정했습니다.(실질적으로 발사 가능)
    프리팹에 스크립트를 붙여 각각의 총알마다 다른 위치 다른 각도로 발사되도록 했습니다.
    3초 뒤에 오브젝트가 비활성화되도록 작성했습니다.

---
+++ Assets/Script/Bullet/bullet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    Vector2 direction;
+    public float speed = 5f;
+
+    void Update()
+    {
+        transform.Translate(direction * speed *  Time.deltaTime);
+    }
+
+    public void Direction(Vector2 dir)
+    {
+        direction = dir.normalized;
+        StartCoroutine(Disable());
+    }
+
+    IEnumerator Disable()
+    {
+        yield return new WaitForSeconds(3f);
+        gameObject.SetActive(false);
+    }
+}

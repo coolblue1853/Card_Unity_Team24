0035616106f33addd489152e51c333872a6e90b1
chae_kkung <ycy0109@naver.com>
2025-04-11 03:27:05 +0900

    feat : 이름을  눌러 블로그로 이동
    - 이름을 누르면 블로그 링크로 이동하는 기능

---
+++ Assets/Script/MW_UI/UI/OpenURL/OpenURL.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour
+{
+    public void OpenYH_URL()
+    {
+        Application.OpenURL("https://sk674494.tistory.com/");
+    }
+
+    public void OpenJN_URL()
+    {
+        Application.OpenURL("https://a-jun.tistory.com/");
+    }
+
+    public void OpenJS_URL()
+    {
+        Application.OpenURL("https://prkjnsk.tistory.com/");
+    }
+
+    public void OpenMW_URL()
+    {
+        Application.OpenURL("https://velog.io/@coolblue/posts");
+    }
+
+    public void OpenCY_URL()
+    {
+        Application.OpenURL("https://velog.io/@ycy0109/posts");
+    }
+}
